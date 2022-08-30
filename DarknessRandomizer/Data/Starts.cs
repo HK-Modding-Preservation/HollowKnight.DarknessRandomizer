e438199f9e05c96a8cc20b8621881727a58dd3c0
dplochcoder <daniel.ploch.coder@gmail.com>
2022-08-30 14:28:38 -0400

    Define startdef safe zones

--- DarknessRandomizer/Data/Starts.cs
+++ DarknessRandomizer/Data/Starts.cs
@@ -10,96 +10,209 @@ namespace DarknessRandomizer.Data
         // Some of these are transition-rando only, and so won't benefit from clusters outside the area
         private static readonly Dictionary<string, HashSet<ClusterName>> ProtectedStartClusters = new()
         {
-            { "Abyss", new() },  // TRANDO only
             {
                 "Ancestral Mound",
                 new()
                 {
+                    ClusterName.CrossroadsFalseKnight,
+                    ClusterName.CrossroadsWest,
+                    ClusterName.CrossroadsEntrance,
+                    ClusterName.CrossroadsLowerPass,
+                    ClusterName.CrossroadsStagHub
                 }
             },
-            { "City Storerooms", new() },
-            { "City of Tears", new() },  // TRANDO only
+            {
+                "City Storerooms",
+                new()
+                {
+                    ClusterName.CityStorerooms,
+                    ClusterName.CityEastEntrance,
+                    ClusterName.CityAboveLemm
+                }
+            },
             {
                 "Crystallized Mound",
                 new()
                 {
+                    ClusterName.CrossroadsOutsideMound,
+                    ClusterName.GroundsMain
                 }
             },
-            { "Distant Village", new() },
             {
                 "East Blue Lake",
                 new()
                 {
+                    ClusterName.GroundsMain,
+                    ClusterName.CityKingsStation,
+                    ClusterName.CityRightHub
                 }
             },
             {
                 "East Crossroads",
                 new()
                 {
+                    ClusterName.CrossroadsCentralPass,
+                    ClusterName.CrossroadsPeaksBridge,
+                    ClusterName.CrossroadsWest,
+                    ClusterName.CrossroadsEntrance,
+                    ClusterName.CrossroadsStagHub,
+                    ClusterName.CrossroadsLowerPass
                 }
             },
             {
                 "East Fog Canyon",
                 new()
                 {
+                    ClusterName.FogCanyonEast,
+                    ClusterName.FungalQueensStation,
+                    ClusterName.FogCanyonEast,
+                    ClusterName.GreenpathLowerHub,
+                    ClusterName.GreenpathLowerPass
                 }
             },
-            { "Far Greenpath", new() },  // TRANDO only
-            { "Fungal Core", new() },  // TRANDO only
-            { "Fungal Wastes", new() },
+            {
+                "Fungal Wastes",
+                new()
+                {
+                    ClusterName.FungalEntrance,
+                    ClusterName.FungalOgres,
+                    ClusterName.FungalUpper,
+                    ClusterName.FungalQueensStation,
+                    ClusterName.FogCanyonWest
+                }
+            },
             {
                 "Greenpath",
                 new()
                 {
+                    ClusterName.GreenpathUpperEast,
+                    ClusterName.GreenpathUpperWest,
+                    ClusterName.GreenpathWest,
+                    ClusterName.GreenpathLowerPass
                 }
             },
             {
                 "Hallownest's Crown",
                 new()
                 {
+                    ClusterName.CrystalPeaksCrown,
+                    ClusterName.CrystalPeaksRoot,
+                    ClusterName.CrystalPeaksUpper,
+                    ClusterName.CrystalPeaksWest,
+                    ClusterName.CrystalPeaksMiddleBridge
                 }
             },
-            { "Hive", new() },  // TRANDO only
             {
                 "King's Pass",
                 new()
                 {
+                    ClusterName.KingsPass,
+                    ClusterName.CrossroadsEntrance,
+                    ClusterName.CrossroadsWest,
+                    ClusterName.CrossroadsPeaksBridge,
+                    ClusterName.CrossroadsLowerPass,
+                    ClusterName.CrossroadsStagHub
                 }
             },
-            { "King's Station", new() },
-            { "Kingdom's Edge", new() },  // TRANDO only
+            {
+                "King's Station",
+                new()
+                {
+                    ClusterName.CityKingsStation,
+                    ClusterName.CityEastEntrance,
+                    ClusterName.CityBridgeToBasin,
+                    ClusterName.BasinFountain
+                }
+            },
             {
                 "Lower Greenpath",
                 new()
                 {
+                    ClusterName.GreenpathMMC,
+                    ClusterName.GreenpathLowerHub,
+                    ClusterName.GreenpathLowerPass,
+                    ClusterName.FogCanyonWest,
+                    ClusterName.FungalQueensStation
                 }
             },
-            { "Mantis Village", new() },
-            { "Outside Colosseum", new() },
-            { "Queen's Gardens", new() },
-            { "Queen's Station", new() },
-            { "Royal Waterways", new() },
+            {
+                "Mantis Village",
+                new()
+                {
+                    ClusterName.FungalMantisVillage,
+                    ClusterName.FungalPilgrimsWay,
+                    ClusterName.FungalElderHuWing,
+                    ClusterName.FungalUpper,
+                    ClusterName.FungalEntrance,
+                    ClusterName.FungalQueensStation
+                }
+            },
+            {
+                "Outside Colosseum",
+                new()
+                {
+                    ClusterName.EdgeColoApproach,
+                    ClusterName.CityKingsStation,
+                    ClusterName.CityRightHub,
+                    ClusterName.EdgeTallRooms,
+                    ClusterName.EdgeEastUpper
+                }
+            },
+            {
+                "Queen's Station",
+                new()
+                {
+                    ClusterName.FungalQueensStation,
+                    ClusterName.FogCanyonWest,
+                    ClusterName.GreenpathLowerHub,
+                    ClusterName.GreenpathLowerPass
+                }
+            },
             {
                 "Stag Nest",
                 new()
                 {
+                    ClusterName.CliffsMain,
+                    ClusterName.KingsPass,
+                    ClusterName.CliffsBaldurShell,
+                    ClusterName.GreenpathUpperWest,
+                    ClusterName.GreenpathWest
                 }
             },
             {
                 "West Blue Lake",
                 new()
                 {
-
+                    ClusterName.CrossroadsShops,
+                    ClusterName.CrossroadsStagHub,
+                    ClusterName.BlueLake,
+                    ClusterName.CrossroadsLowerPass,
+                    ClusterName.CrossroadsWest
                 }
             },
             {
                 "West Crossroads",
                 new()
                 {
+                    ClusterName.CrossroadsMawlek,
+                    ClusterName.CrossroadsMawlekApproach,
+                    ClusterName.CrossroadsWest,
+                    ClusterName.CrossroadsLowerPass,
+                    ClusterName.CrossroadsStagHub,
+                    ClusterName.CrossroadsEntrance
                 }
             },
-            { "West Fog Canyon", new() },
-            { "West Waterways", new() }
+            {
+                "West Fog Canyon",
+                new()
+                {
+                    ClusterName.FogCanyonWest,
+                    ClusterName.FogCanyonWestWing,
+                    ClusterName.FungalQueensStation,
+                    ClusterName.GreenpathLowerHub,
+                    ClusterName.GreenpathLowerPass
+                }
+            }
         };
 
         public static IReadOnlyCollection<ClusterName> GetStartClusters(string start) => ProtectedStartClusters.GetOrDefault(start, () => new());
