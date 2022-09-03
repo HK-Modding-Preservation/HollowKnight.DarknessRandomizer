f50ecd40ac71a4a91c3d799257704d7268279482
dplochcoder <daniel.ploch.coder@gmail.com>
2022-09-03 19:30:33 -0400

    Do some stuff for vanilla shards that doesn't work

--- DarknessRandomizer/Rando/RequestModifier.cs
+++ DarknessRandomizer/Rando/RequestModifier.cs
@@ -10,8 +10,8 @@ namespace DarknessRandomizer.Rando
         public static void Setup()
         {
             RequestBuilder.OnUpdate.Subscribe(-500f, SetupRefs);
+            RequestBuilder.OnUpdate.Subscribe(50.0f, ModifyItems);
             RequestBuilder.OnUpdate.Subscribe(50.0f, RandomizeDarkness);
-            RequestBuilder.OnUpdate.Subscribe(60.0f, AddItems);
         }
 
         private static void SetupRefs(RequestBuilder rb)
@@ -37,7 +37,7 @@ namespace DarknessRandomizer.Rando
             RandoInterop.LS = new(rb.gs, rb.ctx.StartDef);
         }
 
-        private static void AddItems(RequestBuilder rb)
+        private static void ModifyItems(RequestBuilder rb)
         {
             if (!RandoInterop.ShardedLantern || rb.StartItems.GetCount(ItemNames.Lumafly_Lantern) > 0)
             {
@@ -45,8 +45,9 @@ namespace DarknessRandomizer.Rando
                 return;
             }
 
+            var placeholder = $"{PlaceholderItem.Prefix}{ItemNames.Lumafly_Lantern}";
             rb.RemoveItemByName(ItemNames.Lumafly_Lantern);
-            rb.RemoveItemByName($"{PlaceholderItem.Prefix}{ItemNames.Lumafly_Lantern}");
+            rb.RemoveItemByName(placeholder);
 
             if (rb.gs.PoolSettings.Keys)
             {
@@ -55,13 +56,16 @@ namespace DarknessRandomizer.Rando
                 numShards *= rb.gs.DuplicateItemSettings.DuplicateUniqueKeys ? 2 : 1;
                 for (int i = 0; i < numShards; ++i)
                 {
-                    rb.AddItemByName(i < LanternShardItem.TotalNumShards ? LanternShardItem.Name : $"{PlaceholderItem.Prefix}{LanternShardItem.Name}");
+                    rb.AddItemByName(i < LanternShardItem.TotalNumShards ? LanternShardItem.Name : placeholder);
                 }
             }
             else
             {
+                // Ideally, vanilla shards would go in Sly's shop but it just doesn't work and idk why.
+                // It doesn't actually provide any interesting gameplay so no FIXME
                 rb.RemoveFromVanilla(LocationNames.Sly, ItemNames.Lumafly_Lantern);
-
+                rb.RemoveFromVanilla(LocationNames.Sly, placeholder);
+                
                 for (int i = 0; i < LanternShardItem.TotalNumShards; ++i)
                 {
                     VanillaDef def = new(LanternShardItem.Name, LocationNames.Sly, new CostDef[] { new("GEO", 300 + i * 100) });
