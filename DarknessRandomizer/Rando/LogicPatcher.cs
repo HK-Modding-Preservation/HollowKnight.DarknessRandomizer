6aed2cd924aa2104dc7d13690028e6b106123c15
dplochcoder <daniel.ploch.coder@gmail.com>
2022-09-03 13:53:59 -0400

    Move logic overrides into an ephemeral class, to support interop with BenchRando

--- DarknessRandomizer/Rando/LogicPatcher.cs
+++ DarknessRandomizer/Rando/LogicPatcher.cs
@@ -10,168 +10,100 @@ using System.Linq;
 
 namespace DarknessRandomizer.Rando
 {
-    internal static class LogicPatcher
+    internal class LogicOverrides
     {
-        public static void Setup()
-        {
-            RCData.RuntimeLogicOverride.Subscribe(100.0f, ModifyLMB);
-        }
-
         private delegate void LogicOverride(LogicManagerBuilder lmb, string name, LogicClause lc);
 
-        private static readonly Dictionary<string, LogicOverride> LogicOverridesByName = new()
+        private readonly Dictionary<string, LogicOverride> logicOverridesByName;
+        private readonly Dictionary<SceneName, LogicOverride> logicOverridesByTransitionScene;
+        private readonly Dictionary<SceneName, LogicOverride> logicOverridesByUniqueScene;
+
+        public LogicOverrides()
         {
-            // Dream warriors do not appear in dark rooms without lantern.
-            { "Defeated_Elder_Hu", CustomDarkLogicEdit("FALSE") },
-            { "Defeated_Galien", CustomDarkLogicEdit("FALSE") },
-            { "Defeated_Gorb", CustomDarkLogicEdit("FALSE") },
-            { "Defeated_Markoth", CustomDarkLogicEdit("FALSE") },
-            { "Defeated_Marmu", CustomDarkLogicEdit("FALSE") },
-            { "Defeated_No_Eyes", CustomDarkLogicEdit("FALSE") },
-            { "Defeated_Xero", CustomDarkLogicEdit("FALSE") },
-
-            // Dream bosses are coded specially because the checks are located where the dream nail is swung,
-            // but we care whether or not the actual fight room is dark. So we have to account for both rooms.
-            { "Defeated_Failed_Champion", CustomSceneLogicEdit(SceneName.DreamFailedChampion, "FALSE") },
-            { "Defeated_Grey_Prince_Zote", CustomSceneLogicEdit(SceneName.DreamGreyPrinceZote, "FALSE") },
-            { "Defeated_Lost_Kin", CustomSceneLogicEdit(SceneName.DreamLostKin, "DARKROOMS + DIFFICULTSKIPS + PROFICIENTCOMBAT") },
-            { "Defeated_Soul_Tyrant", CustomSceneLogicEdit(SceneName.DreamSoulTyrant, "DARKROOMS + DIFFICULTSKIPS + PROFICIENTCOMBAT") },
-            { "Defeated_White_Defender", CustomSceneLogicEdit(SceneName.DreamWhiteDefender, "DARKROOMS + DIFFICULTSKIPS + PROFICIENTCOMBAT") },
-
-            // Specific checks with difficult platforming.
-            { "Void_Heart", CustomSceneLogicEdit(SceneName.AbyssBirthplace, "DARKROOMS + DIFFICULTSKIPS") },
-
-            // These bosses are deemed difficult in the dark.
-            { "Defeated_Any_Hollow_Knight", CustomDarkLogicEdit("DARKROOMS + DIFFICULTSKIPS + PROFICIENTCOMBAT") },
-            { "Defeated_Any_Nightmare_King", CustomDarkLogicEdit("FALSE") },
-            { "Defeated_Any_Radiance", CustomDarkLogicEdit("FALSE") },
-            { "Defeated_Broken_Vessel", CustomDarkLogicEdit("DARKROOMS + PROFICIENTCOMBAT") },
-            { "Defeated_Brooding_Mawlek", CustomDarkLogicEdit("DARKROOMS + PROFICIENTCOMBAT") },
-            { "Defeated_Collector", CustomDarkLogicEdit("DARKROOMS + DIFFICULTSKIPS + PROFICIENTCOMBAT") },
-            { "Defeated_Colloseum_1", CustomDarkLogicEdit("DARKROOMS + DIFFICULTSKIPS + PROFICIENTCOMBAT") },
-            { "Defeated_Colloseum_2", CustomDarkLogicEdit("FALSE") },
-            { "Defeated_Colloseum_3", CustomDarkLogicEdit("FALSE") },
-            { "Defeated_Crystal_Guardian", CustomDarkLogicEdit("DARKROOMS + PROFICIENTCOMBAT") },
-            { "Defeated_Enraged_Guardian", CustomDarkLogicEdit("DARKROOMS + DIFFICULTSKIPS + PROFICIENTCOMBAT") },
-            { "Defeated_Flukemarm", CustomDarkLogicEdit("DARKROOMS + PROFICIENTCOMBAT") },
-            { "Defeated_Hive_Knight", CustomDarkLogicEdit("DARKROOMS + DIFFICULTSKIPS + PROFICIENTCOMBAT") },
-            { "Defeated_Hornet_Sentinel", CustomDarkLogicEdit("DARKROOMS + PROFICIENTCOMBAT") },
-            { "Defeated_Grimm", CustomDarkLogicEdit("DARKROOMS + PROFICIENTCOMBAT") },
-            { "Defeated_Mantis_Lords", CustomDarkLogicEdit("DARKROOMS + PROFICIENTCOMBAT") },
-            { "Defeated_Nosk", CustomDarkLogicEdit("DARKROOMS + PROFICIENTCOMBAT") },
-            { "Defeated_Pale_Lurker", CustomDarkLogicEdit("DARKROOMS + PROFICIENTCOMBAT") },
-            { "Defeated_Soul_Master", CustomDarkLogicEdit("DARKROOMS + PROFICIENTCOMBAT") },
-            { "Defeated_Traitor_Lord", CustomDarkLogicEdit("DARKROOMS + DIFFICULTSKIPS + PROFICIENTCOMBAT") },
-            { "Defeated_Uumuu", CustomDarkLogicEdit("DARKROOMS + PROFICIENTCOMBAT") },
-            { "Defeated_Watcher_Knights", CustomDarkLogicEdit("DARKROOMS + DIFFICULTSKIPS + PROFICIENTCOMBAT") },
-
-            // Using Elegant Key requires lantern.
-            { $"{SceneName.CityTollBench.Name()}[left3]", CustomDarkLogicEdit("FALSE") },
-
-            // Flower quest simply requires lantern.
-            { "Mask_Shard-Grey_Mourner", (lmb, ln, lc) => lmb.DoLogicEdit(new(ln, "ORIG + LANTERN")) }
-        };
-
-        private static readonly Dictionary<SceneName, LogicOverride> LogicOverridesByTransitionScene = new()
-        {
-            // TODO: Fix for bench rando
-            { SceneName.GreenpathToll, CustomDarkLogicEdit("FALSE") },
-
-            // The following scenes are trivial to navigate while dark, but may contain a check which is
-            // uniquely affected by darkness.
-            { SceneName.FungalQueensStation, NoLogicEdit },
-            { SceneName.GroundsBlueLake, NoLogicEdit },
-            { SceneName.GroundsXero, NoLogicEdit },
-        };
-
-        private static readonly Dictionary<SceneName, LogicOverride> LogicOverridesByUniqueScene = new()
-        {
-            // Checks in these rooms are easy to obtain if the player has isma's tear; there is no danger.
-            { SceneName.GreenpathLakeOfUnn, CustomDarkLogicEdit("ACID") },
-            { SceneName.GreenpathUnn, CustomDarkLogicEdit("ACID") },
-
-            // Checks in these scenes are free, even if dark.
-            { SceneName.BasinCorridortoBrokenVessel, CustomDarkLogicEdit("ANY") },
-            { SceneName.CityTollBench, CustomDarkLogicEdit("ANY") },
-
-            // These scenes have difficult dark platforming.
-            { SceneName.CrystalCrystalHeartGauntlet, CustomDarkLogicEdit("DARKROOMS + DIFFICULTSKIPS") },
-            { SceneName.CrystalDeepFocusGauntlet, CustomDarkLogicEdit("DARKROOMS + DIFFICULTSKIPS") },
-            { SceneName.EdgeWhisperingRoot, CustomDarkLogicEdit("DARKROOMS + DIFFICULTSKIPS") },
-            { SceneName.GreenpathSheoGauntlet, CustomDarkLogicEdit("DARKROOMS + DIFFICULTSKIPS") }
-        };
-
-        private static Dictionary<string, LogicClause> logicCache;
-
-        private static LogicClause GetCachedLogic(string logic)
-        {
-            if (logicCache.TryGetValue(logic, out LogicClause lc))
+            logicOverridesByName = new()
             {
-                return lc;
-            }
+                // Dream warriors do not appear in dark rooms without lantern.
+                { "Defeated_Elder_Hu", CustomDarkLogicEdit("FALSE") },
+                { "Defeated_Galien", CustomDarkLogicEdit("FALSE") },
+                { "Defeated_Gorb", CustomDarkLogicEdit("FALSE") },
+                { "Defeated_Markoth", CustomDarkLogicEdit("FALSE") },
+                { "Defeated_Marmu", CustomDarkLogicEdit("FALSE") },
+                { "Defeated_No_Eyes", CustomDarkLogicEdit("FALSE") },
+                { "Defeated_Xero", CustomDarkLogicEdit("FALSE") },
 
-            lc = new(logic);
-            logicCache[logic] = lc;
-            return lc;
+                // Dream bosses are coded specially because the checks are located where the dream nail is swung,
+                // but we care whether or not the actual fight room is dark. So we have to account for both rooms.
+                { "Defeated_Failed_Champion", CustomSceneLogicEdit(SceneName.DreamFailedChampion, "FALSE") },
+                { "Defeated_Grey_Prince_Zote", CustomSceneLogicEdit(SceneName.DreamGreyPrinceZote, "FALSE") },
+                { "Defeated_Lost_Kin", CustomSceneLogicEdit(SceneName.DreamLostKin, "DARKROOMS + DIFFICULTSKIPS + PROFICIENTCOMBAT") },
+                { "Defeated_Soul_Tyrant", CustomSceneLogicEdit(SceneName.DreamSoulTyrant, "DARKROOMS + DIFFICULTSKIPS + PROFICIENTCOMBAT") },
+                { "Defeated_White_Defender", CustomSceneLogicEdit(SceneName.DreamWhiteDefender, "DARKROOMS + DIFFICULTSKIPS + PROFICIENTCOMBAT") },
+
+                // Specific checks with difficult platforming.
+                { "Void_Heart", CustomSceneLogicEdit(SceneName.AbyssBirthplace, "DARKROOMS + DIFFICULTSKIPS") },
+
+                // These bosses are deemed difficult in the dark.
+                { "Defeated_Any_Hollow_Knight", CustomDarkLogicEdit("DARKROOMS + DIFFICULTSKIPS + PROFICIENTCOMBAT") },
+                { "Defeated_Any_Nightmare_King", CustomDarkLogicEdit("FALSE") },
+                { "Defeated_Any_Radiance", CustomDarkLogicEdit("FALSE") },
+                { "Defeated_Broken_Vessel", CustomDarkLogicEdit("DARKROOMS + PROFICIENTCOMBAT") },
+                { "Defeated_Brooding_Mawlek", CustomDarkLogicEdit("DARKROOMS + PROFICIENTCOMBAT") },
+                { "Defeated_Collector", CustomDarkLogicEdit("DARKROOMS + DIFFICULTSKIPS + PROFICIENTCOMBAT") },
+                { "Defeated_Colloseum_1", CustomDarkLogicEdit("DARKROOMS + DIFFICULTSKIPS + PROFICIENTCOMBAT") },
+                { "Defeated_Colloseum_2", CustomDarkLogicEdit("FALSE") },
+                { "Defeated_Colloseum_3", CustomDarkLogicEdit("FALSE") },
+                { "Defeated_Crystal_Guardian", CustomDarkLogicEdit("DARKROOMS + PROFICIENTCOMBAT") },
+                { "Defeated_Enraged_Guardian", CustomDarkLogicEdit("DARKROOMS + DIFFICULTSKIPS + PROFICIENTCOMBAT") },
+                { "Defeated_Flukemarm", CustomDarkLogicEdit("DARKROOMS + PROFICIENTCOMBAT") },
+                { "Defeated_Hive_Knight", CustomDarkLogicEdit("DARKROOMS + DIFFICULTSKIPS + PROFICIENTCOMBAT") },
+                { "Defeated_Hornet_Sentinel", CustomDarkLogicEdit("DARKROOMS + PROFICIENTCOMBAT") },
+                { "Defeated_Grimm", CustomDarkLogicEdit("DARKROOMS + PROFICIENTCOMBAT") },
+                { "Defeated_Mantis_Lords", CustomDarkLogicEdit("DARKROOMS + PROFICIENTCOMBAT") },
+                { "Defeated_Nosk", CustomDarkLogicEdit("DARKROOMS + PROFICIENTCOMBAT") },
+                { "Defeated_Pale_Lurker", CustomDarkLogicEdit("DARKROOMS + PROFICIENTCOMBAT") },
+                { "Defeated_Soul_Master", CustomDarkLogicEdit("DARKROOMS + PROFICIENTCOMBAT") },
+                { "Defeated_Traitor_Lord", CustomDarkLogicEdit("DARKROOMS + DIFFICULTSKIPS + PROFICIENTCOMBAT") },
+                { "Defeated_Uumuu", CustomDarkLogicEdit("DARKROOMS + PROFICIENTCOMBAT") },
+                { "Defeated_Watcher_Knights", CustomDarkLogicEdit("DARKROOMS + DIFFICULTSKIPS + PROFICIENTCOMBAT") },
+
+                // Using Elegant Key requires lantern.
+                { $"{SceneName.CityTollBench.Name()}[left3]", CustomDarkLogicEdit("FALSE") },
+
+                // Flower quest simply requires lantern.
+                { "Mask_Shard-Grey_Mourner", (lmb, ln, lc) => lmb.DoLogicEdit(new(ln, "ORIG + LANTERN")) }
+            };
+
+            logicOverridesByTransitionScene = new()
+            {
+                // TODO: Fix for bench rando
+                { SceneName.GreenpathToll, CustomDarkLogicEdit("FALSE") },
+
+                // The following scenes are trivial to navigate while dark, but may contain a check which is
+                // uniquely affected by darkness.
+                { SceneName.FungalQueensStation, NoLogicEdit },
+                { SceneName.GroundsBlueLake, NoLogicEdit },
+                { SceneName.GroundsXero, NoLogicEdit },
+            };
+
+            logicOverridesByUniqueScene = new()
+            {
+                // Checks in these rooms are easy to obtain if the player has isma's tear; there is no danger.
+                { SceneName.GreenpathLakeOfUnn, CustomDarkLogicEdit("ACID") },
+                { SceneName.GreenpathUnn, CustomDarkLogicEdit("ACID") },
+
+                // Checks in these scenes are free, even if dark.
+                { SceneName.BasinCorridortoBrokenVessel, CustomDarkLogicEdit("ANY") },
+                { SceneName.CityTollBench, CustomDarkLogicEdit("ANY") },
+
+                // These scenes have difficult dark platforming.
+                { SceneName.CrystalCrystalHeartGauntlet, CustomDarkLogicEdit("DARKROOMS + DIFFICULTSKIPS") },
+                { SceneName.CrystalDeepFocusGauntlet, CustomDarkLogicEdit("DARKROOMS + DIFFICULTSKIPS") },
+                { SceneName.EdgeWhisperingRoot, CustomDarkLogicEdit("DARKROOMS + DIFFICULTSKIPS") },
+                { SceneName.GreenpathSheoGauntlet, CustomDarkLogicEdit("DARKROOMS + DIFFICULTSKIPS") }
+            };
         }
 
-        public static void ModifyLMB(GenerationSettings gs, LogicManagerBuilder lmb)
-        {
-            if (!RandoInterop.IsEnabled()) return;
-
-            logicCache = new();
-            var newResolver = new DarknessVariableResolver(lmb.VariableResolver);
-            lmb.VariableResolver = newResolver;
-
-            // We want to generically modify logic by location (SceneName), but unfortunately the LogicManager is constructed
-            // before any of the location info is provided via the RequestBuilder, so we have to be creative.
-            //
-            // We'll inspect every logic clause for the set of scene transitions it references. If there is only one scene,
-            // we will update the logic accordingly. If there are zero scenes, we ignore it, and if there are two or more, we
-            // require custom handling.
-            //
-            // TODO: Special handling for BenchRando, TheRealJournalRando, possibly others?
-
-            // We defer the edits to avoid messing with dictionary iteration order.
-            List<Action> edits = new();
-            foreach (var e in lmb.LogicLookup)
-            {
-                var name = e.Key;
-                var lc = e.Value;
-                edits.Add(() => EditLogicClause(lmb, name, lc));
-            }
-            edits.ForEach(e => e.Invoke());
-        }
-
-        private static readonly SimpleToken DarkroomsToken = new("DARKROOMS");
-
-        private static void EditLogicClause(LogicManagerBuilder lmb, string name, LogicClause lc)
-        {
-            if (LogicOverridesByName.TryGetValue(name, out LogicOverride handler))
-            {
-                handler.Invoke(lmb, name, lc);
-                return;
-            }
-
-            if (SceneName.IsTransition(name, out SceneName sceneName)
-                && LogicOverridesByTransitionScene.TryGetValue(sceneName, out handler))
-            {
-                handler.Invoke(lmb, name, lc);
-                return;
-            }
-
-            // Check for an inferred scene match.
-            if (InferSingleScene(lc, out SceneName inferred) && LogicOverridesByUniqueScene.TryGetValue(inferred, out handler))
-            {
-                handler.Invoke(lmb, name, lc);
-                return;
-            }
-
-            // No special matches, use the default editor.
-            LogicClauseEditor.EditDarkness(lmb, name, (sink) => sink.Add(DarkroomsToken));
-        }
-
-        private static bool InferSingleScene(LogicClause lc, out SceneName sceneName)
+        private bool InferSingleScene(LogicClause lc, out SceneName sceneName)
         {
             HashSet<SceneName> ret = new();
             foreach (var token in lc)
@@ -196,9 +128,48 @@ namespace DarknessRandomizer.Rando
             return false;
         }
 
-        private static void NoLogicEdit(LogicManagerBuilder lmb, string name, LogicClause lc) { }
+        public bool MaybeInvokeLogicOverride(LogicManagerBuilder lmb, string name, LogicClause lc)
+        {
+            if (logicOverridesByName.TryGetValue(name, out LogicOverride handler))
+            {
+                handler.Invoke(lmb, name, lc);
+                return true;
+            }
 
-        private static LogicOverride CustomDarkLogicEdit(string darkLogic)
+            if (SceneName.IsTransition(name, out SceneName sceneName)
+                && logicOverridesByTransitionScene.TryGetValue(sceneName, out handler))
+            {
+                handler.Invoke(lmb, name, lc);
+                return true;
+            }
+
+            // Check for an inferred scene match.
+            if (InferSingleScene(lc, out SceneName inferred) && logicOverridesByUniqueScene.TryGetValue(inferred, out handler))
+            {
+                handler.Invoke(lmb, name, lc);
+                return true;
+            }
+
+            return false;
+        }
+
+        private Dictionary<string, LogicClause> logicCache = new();
+
+        private LogicClause GetCachedLogic(string logic)
+        {
+            if (logicCache.TryGetValue(logic, out LogicClause lc))
+            {
+                return lc;
+            }
+
+            lc = new(logic);
+            logicCache[logic] = lc;
+            return lc;
+        }
+
+        private void NoLogicEdit(LogicManagerBuilder lmb, string name, LogicClause lc) { }
+
+        private LogicOverride CustomDarkLogicEdit(string darkLogic)
         {
             return (lmb, name, lc) => LogicClauseEditor.EditDarkness(lmb, name, (sink) =>
             {
@@ -210,10 +181,57 @@ namespace DarknessRandomizer.Rando
             });
         }
 
-        private static LogicOverride CustomSceneLogicEdit(SceneName sceneName, string darkLogic)
+        private LogicOverride CustomSceneLogicEdit(SceneName sceneName, string darkLogic)
         {
             return (lmb, name, lc) => lmb.DoLogicEdit(
                 new(name, $"ORIG + ($DarknessLevel[{sceneName}]<2 | LANTERN | {darkLogic})"));
+        }
+    }
+
+    internal static class LogicPatcher
+    {
+        public static void Setup()
+        {
+            RCData.RuntimeLogicOverride.Subscribe(100.0f, ModifyLMB);
+        }
+
+        public static void ModifyLMB(GenerationSettings gs, LogicManagerBuilder lmb)
+        {
+            if (!RandoInterop.IsEnabled()) return;
+
+            var newResolver = new DarknessVariableResolver(lmb.VariableResolver);
+            lmb.VariableResolver = newResolver;
+
+            LogicOverrides overrides = new();
+
+            // We want to generically modify logic by location (SceneName), but unfortunately the LogicManager is constructed
+            // before any of the location info is provided via the RequestBuilder, so we have to be creative.
+            //
+            // We'll inspect every logic clause for the set of scene transitions it references. If there is only one scene,
+            // we will update the logic accordingly. If there are zero scenes, we ignore it, and if there are two or more, we
+            // require custom handling.
+            //
+            // TODO: Special handling for BenchRando, TheRealJournalRando, possibly others?
+
+            // We defer the edits to avoid messing with dictionary iteration order.
+            List<Action> edits = new();
+            foreach (var e in lmb.LogicLookup)
+            {
+                var name = e.Key;
+                var lc = e.Value;
+                edits.Add(() => EditLogicClause(overrides, lmb, name, lc));
+            }
+            edits.ForEach(e => e.Invoke());
+        }
+
+        private static readonly SimpleToken DarkroomsToken = new("DARKROOMS");
+
+        private static void EditLogicClause(LogicOverrides overrides, LogicManagerBuilder lmb, string name, LogicClause lc)
+        {
+            if (overrides.MaybeInvokeLogicOverride(lmb, name, lc)) return;
+
+            // No special matches, use the default editor.
+            LogicClauseEditor.EditDarkness(lmb, name, (sink) => sink.Add(DarkroomsToken));
         }
     }
 }
