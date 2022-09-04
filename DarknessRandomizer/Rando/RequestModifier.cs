d19d3bc57f0e7542ef5a1615278c0a9e25673382
dplochcoder <daniel.ploch.coder@gmail.com>
2022-09-03 21:22:36 -0400

    Fix placeholder logic

--- DarknessRandomizer/Rando/RequestModifier.cs
+++ DarknessRandomizer/Rando/RequestModifier.cs
@@ -37,6 +37,8 @@ namespace DarknessRandomizer.Rando
             RandoInterop.LS = new(rb.gs, rb.ctx.StartDef);
         }
 
+        private static string Placeholder(string item) => $"{PlaceholderItem.Prefix}{item}";
+
         private static void ModifyItems(RequestBuilder rb)
         {
             if (!RandoInterop.ShatteredLantern || rb.StartItems.GetCount(ItemNames.Lumafly_Lantern) > 0)
@@ -45,26 +47,23 @@ namespace DarknessRandomizer.Rando
                 return;
             }
 
-            var placeholder = $"{PlaceholderItem.Prefix}{ItemNames.Lumafly_Lantern}";
             rb.RemoveItemByName(ItemNames.Lumafly_Lantern);
-            rb.RemoveItemByName(placeholder);
+            rb.RemoveItemByName(Placeholder(ItemNames.Lumafly_Lantern));
 
             if (rb.gs.PoolSettings.Keys)
             {
                 int numShards = LanternShardItem.TotalNumShards;
                 numShards += DarknessRandomizer.GS.DarknessRandomizationSettings.TwoDupeShards ? 2 : 0;
                 numShards *= rb.gs.DuplicateItemSettings.DuplicateUniqueKeys ? 2 : 1;
-                for (int i = 0; i < numShards; ++i)
-                {
-                    rb.AddItemByName(i < LanternShardItem.TotalNumShards ? LanternShardItem.Name : placeholder);
-                }
+                rb.AddItemByName(LanternShardItem.Name, LanternShardItem.TotalNumShards);
+                rb.AddItemByName(Placeholder(LanternShardItem.Name), numShards - LanternShardItem.TotalNumShards);
             }
             else
             {
                 // Ideally, vanilla shards would go in Sly's shop but it just doesn't work and idk why.
                 // It doesn't actually provide any interesting gameplay so no FIXME
                 rb.RemoveFromVanilla(LocationNames.Sly, ItemNames.Lumafly_Lantern);
-                rb.RemoveFromVanilla(LocationNames.Sly, placeholder);
+                rb.RemoveFromVanilla(LocationNames.Sly, Placeholder(ItemNames.Lumafly_Lantern));
                 
                 for (int i = 0; i < LanternShardItem.TotalNumShards; ++i)
                 {
