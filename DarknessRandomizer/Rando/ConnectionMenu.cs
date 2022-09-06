5d373c7e1f6dca0c1dc61394d72aaacd4b6dc335
dplochcoder <daniel.ploch.coder@gmail.com>
2022-09-05 23:35:03 -0400

    Soup up the connection menu a bit.

--- DarknessRandomizer/Rando/ConnectionMenu.cs
+++ DarknessRandomizer/Rando/ConnectionMenu.cs
@@ -1,8 +1,10 @@
-﻿using MenuChanger;
+﻿using ItemChanger;
+using MenuChanger;
 using MenuChanger.Extensions;
 using MenuChanger.MenuElements;
 using MenuChanger.MenuPanels;
 using RandomizerMod.Menu;
+using System;
 using static RandomizerMod.Localization;
 
 namespace DarknessRandomizer.Rando
@@ -27,8 +29,21 @@ namespace DarknessRandomizer.Rando
 
         public SmallButton entryButton;
         public MenuPage mainPage;
-        public MenuElementFactory<DarknessRandomizationSettings> elementFactory;
-        public VerticalItemPanel verticalPanel;
+        public GridItemPanel gridPanel;
+
+        private static T Lookup<T>(MenuElementFactory<DarknessRandomizationSettings> factory, string name) where T : MenuItem => factory.ElementLookup[name] as T ?? throw new ArgumentException("Menu error");
+
+        private static void LockIfFalse(MenuItem<bool> src, ILockable dest)
+        {
+            void onChange(bool value)
+            {
+                if (value) dest.Unlock();
+                else dest.Lock();
+            }
+
+            src.ValueChanged += onChange;
+            onChange(src.Value);
+        }
 
         private ConnectionMenu(MenuPage landingPage)
         {
@@ -36,9 +51,21 @@ namespace DarknessRandomizer.Rando
             entryButton = new(landingPage, Localize("Darkness Rando"));
             entryButton.AddHideAndShowEvent(mainPage);
 
-            elementFactory = new(mainPage, DarknessRandomizer.GS.DarknessRandomizationSettings);
-            Localize(elementFactory);
-            verticalPanel = new(mainPage, SpaceParameters.TOP_CENTER_UNDER_TITLE, SpaceParameters.VSPACE_MEDIUM, true, elementFactory.Elements);
+            var settings = DarknessRandomizer.GS.DarknessRandomizationSettings;
+            MenuElementFactory<DarknessRandomizationSettings> factory = new(mainPage, settings);
+            var randomizeDarkness = Lookup<MenuItem<bool>>(factory, nameof(settings.RandomizeDarkness));
+            var darknessLevel = Lookup<MenuItem>(factory, nameof(settings.DarknessLevel));
+            var shatteredLantern = Lookup<MenuItem<bool>>(factory, nameof(settings.ShatteredLantern));
+            var twoDupeShards = Lookup<MenuItem>(factory, nameof(settings.TwoDupeShards));
+
+            LockIfFalse(randomizeDarkness, darknessLevel);
+            LockIfFalse(shatteredLantern, twoDupeShards);
+
+            gridPanel = new(mainPage, SpaceParameters.TOP_CENTER_UNDER_TITLE, 2, SpaceParameters.VSPACE_MEDIUM, SpaceParameters.HSPACE_LARGE, true);
+            gridPanel.Insert(0, 0, randomizeDarkness);
+            gridPanel.Insert(0, 1, shatteredLantern);
+            gridPanel.Insert(1, 0, darknessLevel);
+            gridPanel.Insert(1, 1, twoDupeShards);
         }
     }
 }
